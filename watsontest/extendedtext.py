f61118e6cb924a9ae6263170d3775654c76a8973
josh236 <holdaway236@gmail.com>
2020-01-08 16:59:40 +0000

    for loop issue

--- watsontest/extendedtext.py
+++ watsontest/extendedtext.py
@@ -3,6 +3,11 @@ from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from pandas.io.json import json_normalize
 #from scrape import nextPage
+from random import seed
+from random import randint
+from random import shuffle
+
+seed(1)
 
 authenticator = IAMAuthenticator('4VAC4EecGDVM_t3BvRFvdd-dmwDIdpkbnWffTrhoQKZI')
 tone_analyzer = ToneAnalyzerV3(
@@ -13,18 +18,20 @@ tone_analyzer = ToneAnalyzerV3(
 tone_analyzer.set_service_url('https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com')
 
 text = 'Team, I know that times are tough! Product '\
-   'sales have been disappointing for the past three '\
-  'quarters. We have a competitive product, but we '\
- 'need to do a better job of selling it!'
+       'sales have been disappointing for the past three '\
+        'quarters. We have a competitive product, but we '\
+        'need to do a better job of selling it!'
 
 tone_analysis = tone_analyzer.tone(
     {'text': text},
     content_type='application/json'
 ).get_result()
 
-for i in range(2):
-    myData = tone_analysis['document_tone']['tones'][i]['tone_name']
-    #print (myData)
+for j in range(1):
+    value = randint(0, 2)
+    print(value)
+    myData = tone_analysis['document_tone']['tones'][j]['tone_name']    
+    print (myData)
     
 
 #mydata = json_normalize(tone_analysis['document_tone'])
--- watsontest/surface3d_demo2.py
+++ watsontest/surface3d_demo2.py
@@ -1,4 +1,4 @@
-from mpl_toolkits.mplot3d import a3
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
@@ -20,8 +20,23 @@ if myData == "Sadness":
     z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
 # Plot the surface
     ax.plot_surface(x, y, z, color='b')
+    plt.show()
 
 if myData == "Analytical":
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+
+    z_line = np.linspace(0, 15, 1000)
+    x_line = np.cos(z_line)
+    y_line = np.sin(z_line)
+    ax.plot3D(x_line, y_line, z_line, 'gray')
+
+    z_points = 15 * np.random.random(100)
+    x_points = np.cos(z_points) + 0.1 * np.random.randn(100)
+    y_points = np.sin(z_points) + 0.1 * np.random.randn(100)
+    ax.scatter3D(x_points, y_points, z_points, c=z_points, cmap='hsv');
+
+    plt.show()
 
 
 
